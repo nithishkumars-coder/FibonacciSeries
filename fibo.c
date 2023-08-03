f19393a6974fccd101bfc3fecdff029211e619f2
Selva Gowtham <Selva.Gowtham@solitontech.local>
2023-08-03 10:40:04 +0530

    fixed bugs in generate fibanocci

--- fibo.c
+++ fibo.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-void fibonacci(int n){
-    int fibo[10],i;
+
+
+
+int * fibonacci(int n){
+    int fibo[n],i;
     fibo[0]=0;
     fibo[1]=1;
     for (i=2;i<n;i++)
     {
       fibo[i]=fibo[i-1]+fibo[i-2];
     }
-    for (i=0;i<n;i++)
-    {
-      printf("%d ",fibo[i]);
-    }
+    return fibo;
 }
 int main()
 {
