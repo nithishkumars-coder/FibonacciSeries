da4aa730cd82e90b7ec703ac8593a91d938ae42a
Nithish Sekar <nithish.sekar@solitontech.local>
2023-08-03 10:25:00 +0530

    Fibonacci using Array

--- fibo.c
+++ fibo.c
@@ -1,26 +1,22 @@
-#include<stdio.h>
-void fibonacciSeries(int range)
-{
-   int a=0, b=1, c;
-   while (a<=range)
-   {
-     printf("%d\t", a);
-     c = a+b;
-     a = b;
-     b = c;
-   }
+#include <stdio.h>
+void fibonacci(int n){
+    int fibo[10],i;
+    fibo[0]=0;
+    fibo[1]=1;
+    for (i=2;i<n;i++)
+    {
+      fibo[i]=fibo[i-1]+fibo[i-2];
+    }
+    for (i=0;i<n;i++)
+    {
+      printf("%d ",fibo[i]);
+    }
 }
-
 int main()
 {
-   int range;
-
-   printf("Enter range: ");
-   scanf("%d", &range);
-
-   printf("The fibonacci series is: \n");
-
-   fibonacciSeries(range);
-
+    int n;
+    printf("Enter the Number for fibonacci series : ");
+    scanf("%d",&n);
+    fibonacci(n);
    return 0;
 }
