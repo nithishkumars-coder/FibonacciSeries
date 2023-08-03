91050fed7c35e2894b510e561c500366dd4c7503
Selva Gowtham <Selva.Gowtham@solitontech.local>
2023-08-03 11:06:35 +0530

    Sum Series function

--- fibo.c
+++ fibo.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
+
+int fibo[1000];
+
+int sumSeries(int size){
+  int summ = 0;
+  for(int i = 2; i < size; i++){
+    summ += fibo[i];
+  }
+  printf("%d", summ);
+  return summ;
+}
+
 void fibonacci(int n){
-    int fibo[10],i;
+    int i;
+
     fibo[0]=0;
     fibo[1]=1;
     for (i=2;i<n;i++)
     {
       fibo[i]=fibo[i-1]+fibo[i-2];
     }
-    for (i=0;i<n;i++)
-    {
-      printf("%d ",fibo[i]);
-    }
+
 }
 int main()
 {
@@ -18,5 +28,6 @@ int main()
     printf("Enter the Number for fibonacci series : ");
     scanf("%d",&n);
     fibonacci(n);
+    sumSeries(n);
    return 0;
 }
