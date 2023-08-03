a51596228e1482cce142fd1c0c179be0304576f5
Nithish Sekar <nithish.sekar@solitontech.local>
2023-08-03 11:12:01 +0530

    Fibo conflict changes

--- fibo.c
+++ fibo.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-
 int fibo[1000];
 void fibonacci(int n){
     int i;
-
     fibo[0]=0;
     fibo[1]=1;
     for (i=2;i<n;i++)
     {
       fibo[i]=fibo[i-1]+fibo[i-2];
     }
-    return fibo;
+    for (i=0;i<n;i++)
+    {
+      printf("%d ",fibo[i]);
+    }
 }
 int main()
 {
